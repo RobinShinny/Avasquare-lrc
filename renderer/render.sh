9b4895f2bf9a0f53c7334a57dd2b29a9849d7ed4
ModlyCheeser <160442862+RobinShinny@users.noreply.github.com>
2024-10-19 06:04:08 +0000

    yes

--- renderer/render.sh
+++ renderer/render.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Navigate to the directory where Blender files are located (you can adjust the path)
-cd blend
+# Navigate to the directory where Blender files are located
+cd blend || exit  # Use absolute path or ensure your current path is correct.
 
 # Run Blender with a specific file and rendering parameters
 blend --background nobevels_avatar.blend --render-output //output_#### --render-format PNG
